62f6befbe4230a1d07c6a5437ea68490202c62a1
lamkhjason <162269722+lamkhjason@users.noreply.github.com>
2024-05-03 00:01:59 +0900

    :hammer: Update ChackValue.php
    *Add userId Session function
    *Add comment for each function

--- src/CheckValue.php
+++ src/CheckValue.php
@@ -1,16 +1,28 @@
 <?php 
-  session_start();
+session_start();
 
-  function testInputValue($data) {
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
+//画像をDBに格納できる最大のサイズの定数
+define("MAX_SIZE", 1048576);
 
-  function checkValue($data) {
-    if (empty($data)) {
-      $data = "記入なし";
-    }
-    return $data;
+//異常入出力の確認
+function testInputValue($data) {
+  return htmlspecialchars($data, ENT_QUOTES, "UTF-8");
+}
+
+//プロフィール項目が入力されているかの確認
+function checkProfileItem($data) {
+  if (empty($data)) {
+    $data = "記入なし";
   }
-?>
+  return testInputValue($data);
+}
+
+//ログインIDセッションを設定する
+function setUserIdSession($userId) {
+  $_SESSION["userId"] = $userId;
+}
+
+//ログインIDセッションを返す
+function getUserIdSession() {
+  return $_SESSION["userId"];
+}
