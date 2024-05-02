a4f5eca00364385ac74aaf8a73bbb6541a1d681b
lamkhjason <162269722+lamkhjason@users.noreply.github.com>
2024-05-03 00:52:28 +0900

    :sparkles: Add error message function

--- src/CheckValue.php
+++ src/CheckValue.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 
-//画像をDBに格納できる最大のサイズの定数
+// 画像をDBに格納できる最大のサイズの定数
 define("MAX_SIZE", 1048576);
 
 //異常入出力の確認
@@ -9,7 +9,7 @@ function testInputValue($data) {
   return htmlspecialchars($data, ENT_QUOTES, "UTF-8");
 }
 
-//プロフィール項目が入力されているかの確認
+// プロフィール項目が入力されているかの確認
 function checkProfileItem($data) {
   if (empty($data)) {
     $data = "記入なし";
@@ -17,12 +17,26 @@ function checkProfileItem($data) {
   return testInputValue($data);
 }
 
-//ログインIDセッションを設定する
+// ログインIDのセッションを設定する
 function setUserIdSession($userId) {
   $_SESSION["userId"] = $userId;
 }
 
-//ログインIDセッションを返す
+// セッションに保管されているログインIDを返す
 function getUserIdSession() {
   return $_SESSION["userId"];
+}
+
+// エラーメッセージをセッションに保管
+function setErrorMessage($message) {
+  $_SESSION["error_message"] = $message;
+}
+
+function displayErrorMessage() {
+  if (isset($_SESSION["error_message"])) {
+    // エラーメッセージを表示する
+    echo $_SESSION["error_message"];
+    // エラーメッセージのセッションを解除する
+    unset($_SESSION["error_message"]);
+  }
 }
