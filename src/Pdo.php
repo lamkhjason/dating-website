8f8ed71e7c776a1d6324689b2285a5d30ae6eaae
lamkhjason <162269722+lamkhjason@users.noreply.github.com>
2024-05-02 23:37:16 +0900

    :hammer: Update Pdo.php comment

--- src/Pdo.php
+++ src/Pdo.php
@@ -1,17 +1,16 @@
 <?php
-  $host = 'localhost'; // Hostname
-  $dbname = 'dating_website_db'; // Database name
-  $dbUsername = 'root'; // Username
-  $dbPassword = ''; // Password
+$host = 'localhost'; // ホスト
+$dbname = 'dating_website_db'; // データベース名
+$dbUsername = 'root'; // データベースの利用者名
+$dbPassword = ''; // パスワード
+
+try {
+  $conn = new PDO("mysql:host=$host;dbname=$dbname", $dbUsername, $dbPassword);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   
-  try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $dbUsername, $dbPassword);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    if ($_SERVER['SCRIPT_NAME'] === "/dating-website/src/Pdo.php") {
-      echo "Connected successfully";
-    }
-  } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-  } 
-?>
+  if ($_SERVER['SCRIPT_NAME'] === "/dating-website/src/Pdo.php") {
+    echo "データベース接続成功！";
+  }
+} catch(PDOException $e) {
+  echo "データベース接続失敗：" . $e->getMessage();
+} 
