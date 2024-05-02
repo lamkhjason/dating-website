6b861a3005b656f5c59af62e68ae35cc0c0ac376
kawashir <162565561+kawashir@users.noreply.github.com>
2024-05-02 15:08:03 +0900

    Update LoginStatus.php

--- src/LoginStatus.php
+++ src/LoginStatus.php
@@ -1,37 +1,43 @@
-<?php 
+<?php
   session_start();
 
-  $is_logged_in = isset($_SESSION['user_id']);
-  $userid = $_POST["user_id"];
-
-  if ($is_logged_in)
+  try
   {
-    // データベースからユーザー情報を取得
-    $sql = "SELECT * FROM Users WHERE user_id=?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindValue(1, $userid);
-    $stmt->execute();
-    $user = $stmt->fetch();
+    $is_logged_in = isset($_SESSION['user_id']);
 
-    if (!$user)
+    if ($is_logged_in)
     {
-      // データベース内でユーザーが見つからない場合はログアウト
-      session_destroy();
-      header('Location: Login.php');
-      exit;
+        // データベースからユーザー情報を取得
+        $sql = "SELECT * FROM Users WHERE user_id=?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(1, $is_logged_in);
+        $stmt->execute();
+        $user = $stmt->fetch();
+
+        if (!$user)
+        {
+            // データベース内でユーザーが見つからない場合はログアウト
+            session_destroy();
+            header('Location: Login.php');
+            exit;
+        }
+    }else
+    {
+        // ログインされていない場合はログインページにリダイレクト
+        header('Location: Login.php');
+        exit;
     }
-  } else
+    if (isset($_POST["logoutSubmit"]))
+    {
+        // セッションを破棄
+        session_destroy();
+        // ログインページにリダイレクト
+        header('Location: Login.php');
+        exit;
+    }
+  }catch(PDOException $e)
   {
-    // ログインされていない場合はログインページにリダイレクト
-    header('Location: Login.php');
-    exit;
-  }
-  if (isset($_POST["logoutSubmit"]))
-  {
-    // セッションを破棄
-    session_destroy();
-    // ログインページにリダイレクト
-    header('Location: Login.php');
-    exit;
+      $errorMessage = "ユーザ情報に不具合がありました。";
+      echo $e->getMessage();
   }
 ?>
