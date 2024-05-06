c9137850317e4bda8cf7253ff9776c7d4aead5d4
lamkhjason <162269722+lamkhjason@users.noreply.github.com>
2024-05-06 17:12:54 +0900

    :hammer: Fix CommonTools.php css class and add comment

--- src/CommonTools.php
+++ src/CommonTools.php
@@ -1,4 +1,5 @@
 <?php
+// メニューバーの連想配列
 $menubar = [
   "マッチング一覧" => "/dating-website/src/MatchedList.php",
   "いいね" => "/dating-website/src/Interactions.php",
@@ -7,46 +8,43 @@ $menubar = [
 
 function checkActivePage($directory) {
   if ($directory === $_SERVER["SCRIPT_NAME"] && !isset($_GET["targetUserId"])) {
-    $mode = "btn btn-outline-light active mt-2";
+    $mode = "btn btn-outline-light active my-2"; // 表示している画面のクラス
   } else {
-    $mode = "btn btn-outline-light mt-2";
+    $mode = "btn btn-outline-light my-2"; // 表示している画面以外のクラス
   }
   return $mode;
 }
 ?>
-
+<script src="../js/Congratulations.js"></script>
 <header class="sticky-top container-fluid bg-info p-2">
   <div class="row justify-content-around">
-    <div class="col-auto m-0">
+    <div class="col-auto">
       <i class="bi-calendar-heart-fill text-light" style="font-size: 35px;"></i>
     </div>
     <?php 
+    // ログイン画面と新規登録画面以外、メニューバーとログアウトボタンを表示する
     $showMenubar = 
       $_SERVER["SCRIPT_NAME"] !== "/dating-website/src/Login.php" && 
       $_SERVER["SCRIPT_NAME"] !== "/dating-website/src/Register.php";
     if ($showMenubar):
     ?>
-      <nav class="col-9 m-0">
-        <div class="btn-group container">
-          <?php 
-          include_once("LoginStatus.php");
-          foreach ($menubar as $title => $directory) {
-            $btnClass = checkActivePage($directory);
-            echo "<a href='$directory' class='$btnClass'>$title</a>";
-          }
-          ?>
-        </div>
+      <nav class="col-9 btn-group">
+        <?php 
+        include_once("LoginStatus.php");
+        foreach ($menubar as $title => $directory) {
+          $btnClass = checkActivePage($directory);
+          echo "<a href='$directory' class='$btnClass'>$title</a>";
+        }
+        ?>
       </nav>
-      <div class="col-auto m-0">
-        <form method="POST" action="<?php $_SERVER["SCRIPT_NAME"];?>">
-          <input type="hidden" name="logoutSubmit">
-            <i 
-              class="bi-box-arrow-right text-light" 
-              onclick="this.parentNode.submit()" 
-              style="cursor: pointer; font-size: 35px;"
-            ></i>
-        </form>
-      </div>
+      <form method="POST" class="col-auto">
+        <input type="hidden" name="logoutSubmit">
+        <i 
+          class="bi-box-arrow-right text-light" 
+          onclick="this.parentNode.submit()" 
+          style="cursor: pointer; font-size: 35px;"
+        ></i>
+      </form>
     <?php endif; ?>
   </div>
 </header>
