ac3c4cef87e49f9f7a91ced2b7c9ea8964f5b76a
lamkhjason <162269722+lamkhjason@users.noreply.github.com>
2024-05-06 19:19:34 +0900

    :link: Update url path

---
+++ src/database/LoginStatus.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+include_once("Pdo.php");
+include_once("../components/CheckValue.php");
+
+define("LOGIN_PAGE", "../pages/Login.php");
+
+try {
+  $isLoggingIn = getUserIdSession();
+  if ($isLoggingIn) {
+    // データベースからユーザーID情報を取得
+    $checkUserSql = "SELECT user_id FROM Users WHERE user_id = ?";
+    $stmt = $conn->prepare($checkUserSql);
+    $stmt->bindValue(1, $isLoggingIn);
+    $stmt->execute();
+    
+    $loginUser = $stmt->rowCount();
+    if ($loginUser === 0) {
+      // データベース内でユーザーが見つからない
+      setErrorMessage("ユーザ情報がありません");
+      header("Location:" . LOGIN_PAGE);
+      exit;
+    }
+  } else {
+    // ログインされていない場合はログインページにリダイレクト
+    setErrorMessage("ログインしていません");
+    header("Location:" . LOGIN_PAGE);
+    exit;
+  }
+} catch (PDOException $e) {
+  setErrorMessage("DBエラー：".$e->getMessage());
+  header("Location:" . LOGIN_PAGE);
+  exit;
+}
+
+// ログアウトボタンをクリック
+if (isset($_POST["logoutSubmit"])) {
+  // セッションを破棄
+  session_destroy();
+  // ログインページにリダイレクト
+  header("Location:" . LOGIN_PAGE);
+  exit;
+}
---
+++ src/pages/Login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../../assets/css/Style.css">
+    <title>ログイン画面</title>
+  </head>
+  <body class="bg-info-subtle">
+    <?php 
+    include_once("../components/CheckValue.php");
+    include_once("../components/CommonTools.php"); 
+    ?>
+    <main class="container p-4 mt-4 bg-info-subtle">
+      <form action="../database/ProcessLogin.php" method="POST" class="row g-4 mx-3">
+        <div class="col-12 text-center h2">ログイン</div>
+        <!-- ログインID -->
+        <div class="col-12 px-5">
+          <label for="loginId" class="form-label">ログインID</label>
+          <input 
+            type="text" name="loginId" 
+            class="form-control form-control-lg" 
+          \>
+        </div>
+        <!-- パスワード -->
+        <div class="col-12 px-5 mb-3">
+          <label for="password" class="form-label">パスワード</label>
+          <input 
+            type="password" name="password" 
+            class="form-control form-control-lg" 
+          \>
+        </div>
+        <!-- ログインボタンと新規登録ボタン -->
+        <div class="col-6 d-grid gap-4 mx-auto">
+          <button type="submit" class="btn btn-primary btn-lg" name="loginSubmit">
+            ログイン
+          </button>
+          <a href='Register.php' class="btn btn-dark btn-lg">
+            新規登録
+          </a>
+        </div>
+        <div class="col-12 text-center text-danger">
+          <?php displayErrorMessage();?>
+        </div>
+      </form>
+    </main>
+  </body>
+</html>
