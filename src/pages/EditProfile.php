02df21f5c60d948d51b2d6f06c7eb3222baac128
lamkhjason <162269722+lamkhjason@users.noreply.github.com>
2024-05-06 20:04:08 +0900

    :link: Update EditProfile path and DB

--- src/pages/EditProfile.php
+++ src/pages/EditProfile.php
@@ -1,102 +1,31 @@
-<?php
-include_once("Pdo.php");
-include_once("CheckValue.php");
-include_once("SelectProfileItem.php");
-
-function profileTextField($itemValue, $itemTitle, $itemKey) {
-  echo "<label for='$itemKey' class='form-label'>$itemTitle</label>";
-  echo "<input type='text' class='form-control form-control-lg' 
-    name='$itemKey' value='$itemValue'>";
-}
-function profileTextArea($itemValue, $itemTitle, $itemKey) {
-  echo "<label for='$itemKey' class='form-label'>$itemTitle</label>";
-  echo "<textarea class='form-control form-control-lg' 
-    name='$itemKey'>$itemValue</textarea>";
-}
-
-if (isset($_POST["editProfileSubmit"])) {
-  // ユーザ名、性別と年齢が入力されているか
-  $inputValue = !empty($_POST["username"]) && isset($_POST["gender"]) && 
-    ($_POST["age"] !== "年齢を選択していください");
-  if ($inputValue) {
-    try {
-      // トランザクション開始
-      $conn->beginTransaction();
-      // 編集したプロフィールを更新
-      $updateProfileSql = "UPDATE Users SET username = ?, age = ?, gender = ?,
-        blood_type = ?, location = ?, interests = ?, description = ? WHERE user_id = ?";
-      $stmt = $conn->prepare($updateProfileSql);
-      $count = 1;
-      foreach ($_POST as $postKey => $postValue) {
-        if ($postKey !== "editProfileSubmit" && $postKey !== "profilePicture") {
-          $postValue = testInputValue($postValue);
-          $stmt->bindValue($count, $postValue);
-          $count++;
-        }
-      }
-      $stmt->execute();
-      // プロフィール写真を更新しているか
-      $uploadPicture = is_uploaded_file($_FILES["profilePicture"]["tmp_name"]);
-      if ($uploadPicture) {
-        // 画像情報を取得
-        $pictureName = $_FILES["profilePicture"]["name"];
-        $pictureType = $_FILES["profilePicture"]["type"];
-        $pictureSize = $_FILES["profilePicture"]["size"];
-        $pictureTmpName = $_FILES["profilePicture"]["tmp_name"];
-        $pictureFile = file_get_contents($pictureTmpName);
-        $pictureContents = base64_encode($pictureFile);
-        
-        // アップロードした画像のサイズ確認
-        if ($pictureSize <= MAX_SIZE) {
-          //DBを更新する 
-          $updatePictureSql = 
-            "UPDATE Profile_Pictures SET picture_name = ?, picture_type = ?, 
-            picture_contents = ? WHERE user_id = ?";
-          $stmt = $conn->prepare($updatePictureSql);
-          $stmt->bindValue(1, $pictureName);
-          $stmt->bindValue(2, $pictureType);
-          $stmt->bindValue(3, $pictureContents);
-          $stmt->bindValue(4, getUserIdSession());
-          $stmt->execute();
-          
-          // 画像とプロフィール情報が登録成功したらコミットする
-          $conn->commit();
-          header("Location: Profile.php");
-          exit;
-        } else {
-          // サイズが1Mを超えたらロールバックする
-          setErrorMessage("画像サイズが1Mを超えました");
-          $conn->rollback();
-        }
-      } else {
-        // プロフィール情報が登録成功したらコミットする
-        $conn->commit();
-        header("Location: Profile.php");
-        exit;
-      }
-    } catch (Exception $e) {
-      setErrorMessage("プロフィール更新失敗: " . $e->getMessage());
-      // 片方失敗したらロールバックする
-      $conn->rollback();
-    }
-  } else {
-    setErrorMessage("名前、年齢、性別が必須項目です");
-  }
-}
-?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="../css/Style.css">
+    <link rel="stylesheet" href="../../assets/css/Style.css">
     <title>プロフィール編集画面</title>
   </head>
   <body class="bg-info-subtle">
-    <?php include_once("CommonTools.php"); ?>
+    <?php
+    include_once("../components/CheckValue.php");
+    include_once("../database/SelectProfileItem.php");
+    include_once("../components/CommonTools.php");
+    
+    function profileTextField($itemValue, $itemTitle, $itemKey) {
+      echo "<label for='$itemKey' class='form-label'>$itemTitle</label>";
+      echo "<input type='text' class='form-control form-control-lg' 
+        name='$itemKey' placeholder='$itemTitle"."を入力して下さい' value='$itemValue'>";
+    }
+    function profileTextArea($itemValue, $itemTitle, $itemKey) {
+      echo "<label for='$itemKey' class='form-label'>$itemTitle</label>";
+      echo "<textarea class='form-control form-control-lg' name='$itemKey' 
+        placeholder='$itemTitle"."を入力して下さい'>$itemValue</textarea>";
+    }
+    ?>
     <main class="container p-4 bg-info-subtle">
-      <form class="row g-4" method="POST" action="EditProfile.php">
+      <form class="row g-4" method="POST" action="../database/UpdateEditProfile.php">
         <!-- 名前 -->
         <div class="col-md-7">
           <?php profileTextField($profileItem['username'], "名前", "username"); ?>
