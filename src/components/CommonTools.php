876a99f3ab01cc51dbe5895fe606021de1821a9f
lamkhjason <lamkh@cocoto.co.jp>
2024-05-17 18:28:05 +0900

    :hammer: change changing menubar link

--- src/components/CommonTools.php
+++ src/components/CommonTools.php
@@ -26,8 +26,9 @@ function checkActivePage($directory) {
     <?php 
     // ログイン画面と新規登録画面以外、メニューバーとログアウトボタンを表示する
     $showMenubar = 
-      $_SERVER["SCRIPT_NAME"] !== "/dating-website/src/pages/Login.php" && 
-      $_SERVER["SCRIPT_NAME"] !== "/dating-website/src/pages/Register.php";
+      $_SERVER["SCRIPT_NAME"] === "マッチング一覧" || 
+      $_SERVER["SCRIPT_NAME"] === "いいね" ||
+      $_SERVER["SCRIPT_NAME"] === "プロフィール";
     if ($showMenubar):
     ?>
       <nav class="col-9 btn-group">
