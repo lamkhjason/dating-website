3d05d5aba26f07a20b546c0a207b2aa0bad531d5
lamkhjason <162269722+lamkhjason@users.noreply.github.com>
2024-05-23 03:33:14 +0900

    :art: customise menubar btn classes

--- src/components/CommonTools.php
+++ src/components/CommonTools.php
@@ -2,26 +2,22 @@
 // ファイル名： CommonTool.php
 // コード内容： 各画面の共通部分（header）
 
+define("SYSTEM_NAME", "/dating-website/src");
 // メニューバーの連想配列
 $menubar = [
-  "マッチング一覧" => "/dating-website/src/pages/MatchedList.php",
-  "いいね" => "/dating-website/src/pages/Interactions.php",
-  "プロフィール" => "/dating-website/src/pages/Profile.php",
+  "マッチング一覧" => SYSTEM_NAME . "/pages/MatchedList.php",
+  "いいね" => SYSTEM_NAME . "/pages/Interactions.php",
+  "プロフィール" => SYSTEM_NAME . "/pages/Profile.php",
 ];
 
 function checkActivePage($directory) {
   if ($directory === $_SERVER["SCRIPT_NAME"] && !isset($_GET["targetUserId"])) {
-    $mode = "btn btn-outline-light active my-2"; // 表示している画面のクラス
-  } else {
-    $mode = "btn btn-outline-light my-2"; // 表示している画面以外のクラス
+    return ":active"; // 表示している画面をactiveモードにする
   }
-  return $mode;
 }
 ?>
 <header class="top-bar">
-  <div class="col-auto">
-    <i class="bi-calendar-heart-fill system-icon"></i>
-  </div>
+  <div class="system-icon"><i class="bi-calendar-heart-fill"></i></div>
   <?php 
   // ログイン画面と新規登録画面以外、メニューバーとログアウトボタンを表示する
   $showMenubar = 
@@ -30,21 +26,20 @@ function checkActivePage($directory) {
     $_SERVER["SCRIPT_NAME"] === $menubar['マッチング一覧'];
   if ($showMenubar):
   ?>
-    <nav class="col-9 btn-group">
+    <nav class="nav-btn-group">
       <?php 
       include_once("../database/LoginStatus.php");
       foreach ($menubar as $title => $directory) {
-        $btnClass = checkActivePage($directory);
-        echo "<a href='$directory' class='$btnClass'>$title</a>";
+        $activeMode = checkActivePage($directory);
+        echo "<a href='$directory' class='nav-btn' $activeMode>$title</a>";
       }
       ?>
     </nav>
-    <form method="POST" class="col-auto">
+    <form method="POST" class="logout-icon">
       <input type="hidden" name="logoutSubmit">
       <i 
-        class="bi-box-arrow-right text-light" 
+        class="bi-box-arrow-right" 
         onclick="this.parentNode.submit()" 
-        style="cursor: pointer; font-size: 35px;"
       ></i>
     </form>
   <?php endif; ?>
