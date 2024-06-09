9a4063af4e281efb8d1a3ac0ed950c45262f9581
lamkhjason <162269722+lamkhjason@users.noreply.github.com>
2024-06-10 00:30:51 +0900

    :hammer: Fix interactions spelling

--- src/database/SelectInteractions.php
+++ src/database/SelectInteractions.php
@@ -12,7 +12,7 @@ try {
   $interactionListSql = 
   "SELECT u.user_id, u.username, u.gender, u.age, p.picture_contents, p.picture_type
   FROM Users u LEFT JOIN Profile_Pictures p ON u.user_id = p.user_id
-  LEFT JOIN interactions i ON u.user_id = i.target_user_id AND i.user_id = :LoginId
+  LEFT JOIN Interactions i ON u.user_id = i.target_user_id AND i.user_id = :LoginId
   WHERE i.user_id IS NULL AND u.user_id != :LoginId;";
   $stmt = $conn->prepare($interactionListSql);
   $stmt->bindValue(':LoginId', $loginUserId, PDO::PARAM_INT);
