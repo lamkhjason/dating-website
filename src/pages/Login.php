b7bbb8f63283cd940eb1bd13219caaef55893eff
lamkhjason <162269722+lamkhjason@users.noreply.github.com>
2024-06-15 00:58:56 +0900

    :hammer: Update html head

--- src/pages/Login.php
+++ src/pages/Login.php
@@ -6,8 +6,8 @@
 <html lang="ja">
   <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="../assets/css/Style.css">
     <title>ログイン画面</title>
   </head>
