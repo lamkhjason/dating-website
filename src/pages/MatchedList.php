a2ec48c0c7a967bd3906a99d9272e48b95e828ef
lamkhjason <162269722+lamkhjason@users.noreply.github.com>
2024-06-15 03:37:28 +0900

    :lipstick: Add search icon

--- src/pages/MatchedList.php
+++ src/pages/MatchedList.php
@@ -23,7 +23,9 @@
           type="text" name="SearchUserName" value="<?php echo $input; ?>"
           class="search-input-area" placeholder="名前を入力してくだい"
         >
-        <button type="submit" class="search-btn" name="SearchSubmit">検索</button>
+        <button type="submit" class="search-btn" name="SearchSubmit">
+          <i class="bi bi-search"></i> 検索
+        </button>
       </form>
       <div class="error-message"><?php displayErrorMessage();?></div>
       <?php 
