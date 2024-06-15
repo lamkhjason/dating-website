0bad76579856647367759b47189ad9950314eb72
lamkhjason <162269722+lamkhjason@users.noreply.github.com>
2024-06-15 17:21:00 +0900

    :card_file_box: Update SQL

--- src/database/SelectProfileItem.php
+++ src/database/SelectProfileItem.php
@@ -21,7 +21,8 @@ try {
   // 表示するユーザのプロフィールを取得
   $profileSql = 
     "SELECT u.username, u.gender, u.age, u.blood_type, u.location, u.interests, 
-    u.description, pp.picture_contents, pp.picture_type FROM Users u 
+    u.description, u.height, u.weight, u.education, u.occupation, u.smoking_habits,
+    u.drinking_habits, pp.picture_contents, pp.picture_type FROM Users u 
     LEFT JOIN Profile_Pictures pp ON u.user_id = pp.user_id WHERE u.user_id = ?";
   $stmt = $conn->prepare($profileSql);
   $stmt->bindValue(1, $displayUserId);
@@ -39,5 +40,12 @@ $bloodType = testInputValue($profileItem['blood_type']);
 $location = testInputValue($profileItem['location']);
 $interests = testInputValue($profileItem['interests']);
 $description = testInputValue($profileItem['description']);
+$height = testInputValue($profileItem['height']);
+$weight = testInputValue($profileItem['weight']);
+$education = testInputValue($profileItem['education']);
+$occupation = testInputValue($profileItem['occupation']);
+$smokingHabits = testInputValue($profileItem['smoking_habits']);
+$drinkingHabits = testInputValue($profileItem['drinking_habits']);
+
 $pictureContents = testInputValue($profileItem['picture_contents']);
 $pictureType = testInputValue($profileItem['picture_type']);
