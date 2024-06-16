9efd95c4ade2467c7a6ea2c37d8b1d67d891bf0e
lamkhjason <162269722+lamkhjason@users.noreply.github.com>
2024-06-16 21:47:35 +0900

    :lipstick: Add favicon to each pages

--- src/pages/Interactions.php
+++ src/pages/Interactions.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" href="../assets/icon/calendar-heart-fill.svg">
     <link rel="stylesheet" href="../assets/css/Style.css">
     <title>いいね画面</title>
   </head>
